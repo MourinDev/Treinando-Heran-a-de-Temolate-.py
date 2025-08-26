01aa3a729f446675fa2fc7f4f54d10cc50394dbb
Felipe de Moura Cavalcanti <df91353ps@df.trf1.gov.br>
2025-08-26 08:50:08 -0300

    alteracao_urls

--- tarefas/views.py
+++ tarefas/views.py
@@ -4,4 +4,8 @@ from django.http import HttpResponse
 def home (request):
     frase = "Hello, world! This is my first Django app."
     return HttpResponse(frase)
+def add_task(request):
+    task = "New Task"
+    return HttpResponse(task)
+
 
